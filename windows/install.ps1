434668c4feb875e2faac522bc2c5bb052b4f9751
Simon Prochazka <simon@fallion.net>
2019-05-25 18:11:22 +0200

    Add Windows

---
+++ windows/install.ps1
@@ -0,0 +1,28 @@
+### Chocolatey
+Write-Host "Installing Desktop Utilities..." -ForegroundColor "Yellow"
+if ((which cinst) -eq $null) {
+    iex (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
+    Refresh-Environment
+    choco feature enable -n=allowGlobalConfirmation
+}
+
+# system and cli
+choco install curl                --limit-output
+choco install git.install         --limit-output -params '"/GitAndUnixToolsOnPath /NoShellIntegration"'
+choco install nvm.portable        --limit-output
+choco install golang              --limit-output
+
+# browser
+choco install brave  --limit-output
+
+# devs tools
+choco install vscode --limit-output
+
+Refresh-Environment
+
+# nvm set up 
+nvm on
+$nodeLtsVersion = choco search nodejs-lts --limit-output | ConvertFrom-String -TemplateContent "{Name:package-name}\|{Version:1.11.1}" | Select -ExpandProperty "Version"
+nvm install $nodeLtsVersion
+nvm use $nodeLtsVersion
+Remove-Variable nodeLtsVersion

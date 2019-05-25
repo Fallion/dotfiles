5713c90ab321b36c497097669ab66f84b8f28f57
Simon Prochazka <simon@fallion.net>
2019-05-26 01:23:42 +0200

    Add missing pre-steps

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,3 +1,32 @@
+# Check to see if we are currently running "as Administrator"
+if (!(Verify-Elevated)) {
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    $newProcess.Verb = "runas";
+    [System.Diagnostics.Process]::Start($newProcess);
+ 
+    exit
+ }
+ 
+ 
+ ### Update Help for Modules
+ Write-Host "Updating Help..." -ForegroundColor "Yellow"
+ Update-Help -Force
+ 
+ 
+ ### Package Providers
+ Write-Host "Installing Package Providers..." -ForegroundColor "Yellow"
+ Get-PackageProvider NuGet -Force | Out-Null
+ # Chocolatey Provider is not ready yet. Use normal Chocolatey
+ #Get-PackageProvider Chocolatey -Force
+ #Set-PackageSource -Name chocolatey -Trusted
+ 
+ 
+ ### Install PowerShell Modules
+ Write-Host "Installing PowerShell Modules..." -ForegroundColor "Yellow"
+ Install-Module Posh-Git -Scope CurrentUser -Force
+ Install-Module PSWindowsUpdate -Scope CurrentUser -Force 
+
 ### Chocolatey
 Write-Host "Installing Desktop Utilities..." -ForegroundColor "Yellow"
 if ((which cinst) -eq $null) {

21fbe0363cb8c73c24f9341c7d816543df3d23be
Fallion <simon@fallion.net>
2020-03-02 14:42:24 +0100

    remove minikube, add notion and spectacles

--- setup.sh
+++ setup.sh
@@ -1,105 +1,75 @@
 #!/usr/bin/env bash
 
-if [ "$(uname)" == "Darwin" ]; then
-	# Ask for the administrator password upfront
-	sudo -v
+# Ask for the administrator password upfront
+sudo -v
 
-	# Check for Homebrew and install it if missing
-	if test ! $(which brew)
-	then
-		echo "Installing Homebrew..."
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	fi
-
-	# Make sure we’re using the latest Homebrew
-	brew update
-	# Upgrade any already-installed formulae
-	brew upgrade --all
-
-	# Add tap for Dart
-	brew tap dart-lang/dart
-
-	echo "Installing Brew apps..."
-	apps=(
-		git
-        go
-		n
-		node
-		python
-		python3
-		wget
-		zsh
-		make
-		dart
-		kubernetes-cli
-		protobuf
-	)
-	brew install "${apps[@]}"
-	sudo easy_install pip
-
-	# Install Caskroom
-	brew tap caskroom/cask
-	brew tap caskroom/versions
-
-	echo "Installing Brew Casks..."
-	apps=(
-		gitkraken
-		cyberduck
-		docker
-		discord
-		brave-browser
-		insomnia
-		slack
-		spotify
-		# steam
-		visual-studio-code
-		1password
-		graphql-playground
-		now
-		electron
-		google-cloud-sdk
-		minikube
-		keybase
-		bloomrpc
-	)
-	brew cask install "${apps[@]}"
-
-	# Remove outdated versions from the cellar
-	brew cleanup
-
-	# Set screencapture location
-	defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
-	killall SystemUIServer
-
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	# Ask for the administrator password upfront
-	sudo -v
-
-	# Make sure we’re using the latest repositories
-	sudo apt update
-	sudo apt install snapd
-
-	apps=(
-		go
-		node
-        spotify
-		code
-		slack
-		gitkraken
-		docker
-		brave
-		insomnia
-	)
-	for app in "${apps[@]}"
-	do
-		sudo snap install "${app}" --classic
-	done
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+	echo "Installing Homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# Make sure we’re using the latest Homebrew
+brew update
+# Upgrade any already-installed formulae
+brew upgrade --all
+
+# Add tap for Dart
+brew tap dart-lang/dart
+
+echo "Installing Brew apps..."
+apps=(
+	git
+	go
+	n
+	node
+	python
+	python3
+	wget
+	zsh
+	make
+	dart
+	kubernetes-cli
+	protobuf
+)
+brew install "${apps[@]}"
+sudo easy_install pip
+
+# Install Caskroom
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+echo "Installing Brew Casks..."
+apps=(
+	gitkraken
+	docker
+	brave-browser
+	insomnia
+	slack
+	spotify
+	visual-studio-code
+	1password
+	electron
+	google-cloud-sdk
+	keybase
+	bloomrpc
+	spectacle
+	notion
+)
+brew cask install "${apps[@]}"
+
+# Remove outdated versions from the cellar
+brew cleanup
+
+# Set screencapture location
+defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
+killall SystemUIServer
 
 cd "$(dirname "${BASH_SOURCE}")";
 
 git init
-git remote add origin git@github.com:Fallion/dotfiles.git
+git remote add origin git@github.com:fallion/dotfiles.git
 
 # Symlink dotfiles
 git pull origin master;
